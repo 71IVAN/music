890e2d9359fe45fef025883d0796102ba87cd4f5
ivan <71arrieta71@gmail.com>
2023-04-24 21:39:24 -0500

    inicio

---
+++ src/History/History.jsx
@@ -0,0 +1,47 @@
+//import 'History.css'
+export function History(){
+    return(
+        <>
+        <div className="container">
+            <div className="row">
+                <div className="col-12 col-md-6">
+                    <h2>PostMalone</h2>
+                    <p>Post Malone es uno de los raperos más sugerentes y genuinos del momento. Aunque su sonido bebe del trap y del hip-hop sureño, en sus canciones siempre sobresalen una emoción, un trasfondo soul y una mirada pop absolutamente deslumbrantes. El cantante y productor neoyorquino es una fábrica imparable de hits, como «Congratulations» o «rockstar», pero además es el creador de una fórmula musical novedosa e impactante que ha estallado en discos como Stoney (2016). Dale al play y entenderás por qué todo el mundo habla de Post Malone.</p>
+                    <img src="https://firebasestorage.googleapis.com/v0/b/music-2b7e1.appspot.com/o/foto1.jpg?alt=media&token=de052e3b-7320-45c1-b8b9-b2ccec2e697e" alt="foto1" className='img-fluid w-100'  />
+                </div>
+                <div className="col-12 col-md-3">
+                    <h3>Primeros años de vida</h3>
+                    <p>Post Malone (4 de julio de 1995) cantante, productor y compositor. Nació en Siracusa, Nueva York, Estados Unidos. Su nombre de nacimiento es Austin Richard Post. Desde muy niño su familia se mudó a Grapevine, Texas. En sus tiempos libres jugaba baloncesto. Su padre era vendedor de comidas del equipo Cowboys de Dallas, esto permitió que Malone ingresara a todos los partidos de manera gratuita y estuviera cerca de los jugadores. A los 12 años su madre le regaló una guitarra. Recibió influencia de Metallica y Megadeth, Malone hizo un tránsito entre el hardcore, el rock clásico y el Hip hop y Trap.
+
+                    
+                    Biografía de Post Malone
+                    Post Malone (4 de julio de 1995) cantante, productor y compositor. Nació en Siracusa, Nueva York, Estados Unidos. Su nombre de nacimiento es Austin Richard Post. Desde muy niño su familia se mudó a Grapevine, Texas. En sus tiempos libres jugaba baloncesto. Su padre era vendedor de comidas del equipo Cowboys de Dallas, esto permitió que Malone ingresara a todos los partidos de manera gratuita y estuviera cerca de los jugadores. A los 12 años su madre le regaló una guitarra. Recibió influencia de Metallica y Megadeth, Malone hizo un tránsito entre el hardcore, el rock clásico y el Hip hop y Trap.
+
+                    <img src="https://firebasestorage.googleapis.com/v0/b/music-2b7e1.appspot.com/o/foto2.jpg?alt=media&token=0c858a88-c8e0-4c62-acfc-bd2b4ad65a2f" alt="foto2" className="img-fluid"/>
+
+                    Empezó una carrera universitaria en Tarrant County College pero no la terminó porque Malone se trasladó a Los Ángeles, California, con su viejo amigo Jason Stokes. Durante ese tiempo tuvo la oportunidad de conocer a 1st y Rich de FKi y Rex Kudo, hicieron trabajos de producción en varios temas de Malone: White Iverson, es una referencia al jugador de básquet profesional Allen Iverson. Para mitad del año 2015, se lanzó el vídeo musical oficial, recibió más de 613 millones de visualizaciones. Post lanzó otros sencillos desde su SoundCloud: Too Young, What’s Up, Patient y Tear$.  Congratulations y rockstar (las canciones con más visitas en YouTube) respectivamente alcanzaron los números 8 y 1 en el Billboard Hot 100, de los Estados Unidos.
+
+                    Post rápidamente atrajo la atención de las discográficas. Como consecuencia, firmó un contrato de grabación con Republic Records y empezó a trabajar con importantes estrellas del rap tales como 50 Cent, Young Thoug, y Kanye West. Malone fue invitado para realizar un acto de apertura para el Bieber’s Purpose World Tour. Go Flex fue su nuevo sencillo del 2016. Posteriormente, publicó un mixtape titulado August 26. Malone interpretó a Go Flex en Jimmy Kimmel Live!.
+
+                    </p>
+                </div>
+
+                <div className="col-12 col-md-3">
+                    <h3>Hip-Hop</h3>
+                    <p>
+                    Se le ha llamado el Donald Trump de hip-hop por sus letras misóginas y por su apoyo a la segunda enmienda norteamericana al Derecho a poseer armas, asuntos que deberían haber acabado con su carrera musical, aunque lo hicieron más popular. Al rapero estadounidense también se le acusa de ser un niño rico cuyos padres le pagaron su carrera musical.
+
+                    <img src="https://firebasestorage.googleapis.com/v0/b/music-2b7e1.appspot.com/o/foto3.jpg?alt=media&token=0e53ad98-e96f-4b43-86e5-6bacacb84eec" alt="foto3" className="img-fluid"/>
+
+
+                    Se le considera un híbrido de rapero con tendencia al pop y cadencia de música country. Sus letras simbolizan lo oscuro y desesperado que vive el hip-hop contemporáneo.
+                    </p>
+                </div>
+
+            </div>
+        </div>
+          
+        </>
+    )
+
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import  'bootstrap/dist/js/bootstrap.min.js'
+import  'bootstrap/dist/css/bootstrap.min.css'
+import {Home} from "../src/Home/Home"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Home/>
+  </React.StrictMode>,
+)
